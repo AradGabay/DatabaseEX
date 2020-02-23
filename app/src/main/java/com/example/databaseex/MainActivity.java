1403149d74a515a86c615c92828c134514669125
liemb <lb6031@bs.amalnet.k12.il>
2020-02-23 13:22:25 +0200

    another commit

--- app/src/main/java/com/example/databaseex/MainActivity.java
+++ app/src/main/java/com/example/databaseex/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.databaseex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
     HelperDB hlp;
-    EditText studname, studphone, homephone, address, momsname, dadsname, dadsphone, momsphone;
-    EditText gradesubj, gradenum,quarter;
+    EditText studname, studphone, homephone, eTaddress, momsname, dadsname, dadsphone, momsphone;
+    EditText gradesubj, gradenum, gradequarter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         studname = findViewById(R.id.studname);
         studphone = findViewById(R.id.phonenumber);
         homephone = findViewById(R.id.homephone);
-        address = findViewById(R.id.address);
+        eTaddress = findViewById(R.id.address);
         momsname = findViewById(R.id.momsname);
         momsphone = findViewById(R.id.momsphone);
         dadsname = findViewById(R.id.dadsname);
@@ -30,18 +33,89 @@ public class MainActivity extends AppCompatActivity {
 
         gradesubj = findViewById(R.id.gradesubj);
         gradenum = findViewById(R.id.grade);
-        quarter = findViewById(R.id.quarter);
+        gradequarter = findViewById(R.id.quarter);
 
         hlp = new HelperDB(this);
 
 
     }
 
-
+        //enter student's data into students database
     public void enterStudentData(View view) {
+        String name, phone, home, address, mom, momphone, dad, dadphone;
+
+        name = studname.getText().toString();
+        phone = studphone.getText().toString();
+        home = homephone.getText().toString();
+        address = eTaddress.getText().toString();
+        mom = momsname.getText().toString();
+        momphone = momsphone.getText().toString();
+        dad = dadsname.getText().toString();
+        dadphone = dadsphone.getText().toString();
+
+
+        ContentValues cv = new ContentValues();
+        cv.put(Students.NAME,name);
+        cv.put(Students.ADDRESS,address);
+        cv.put(Students.PHONENUMBER,phone);
+        cv.put(Students.HOMEPHONE,home);
+        cv.put(Students.MOMNAME,mom);
+        cv.put(Students.MOMPHONE,momphone);
+        cv.put(Students.DADNAME,dad);
+        cv.put(Students.DADPHONE,dadphone);
+
+        db = hlp.getWritableDatabase();
+        db.insert(Students.TABLE_STUDENTS,null,cv);
+        db.close();
+
 
     }
-
+        //enter student's grade data into grades database
     public void enterGradeData(View view) {
+        String subject;
+        int grade,quarter;
+
+        subject = gradesubj.getText().toString();
+        grade = Integer.parseInt(gradenum.getText().toString());
+        quarter = Integer.parseInt(gradequarter.getText().toString());
+
+        ContentValues cv = new ContentValues();
+        cv.put(Grades.NAME,subject);
+        cv.put(Grades.GRADE,grade);
+        cv.put(Grades.QUARTER,quarter);
+
+        db = hlp.getWritableDatabase();
+        db.insert(Grades.TABLE_GRADES, null, cv);
+        db.close();
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case (R.id.menuWatch): {
+                Intent t = new Intent(this, Watchtables.class);
+                startActivity(t);
+                break;
+            }
+            case (R.id.menuUpdate): {
+                Intent t = new Intent(this, Update.class);
+                startActivity(t);
+                break;
+            }
+            case (R.id.menuSort): {
+                Intent t = new Intent(this, Sort.class);
+                startActivity(t);
+                break;
+            }
+            default:
+                break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/databaseex/Watchtables.java
@@ -0,0 +1,14 @@
+package com.example.databaseex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Watchtables extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watchtables);
+    }
+}
